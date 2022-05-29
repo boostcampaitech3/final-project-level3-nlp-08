22fa57429c063c96db05986fc73f4b4e6a62a1aa
DJ_IM <idj7183@naver.com>
2022-05-29 16:50:08 +0900

    모듈화

--- summarization/kobart/data_loader/processing.py
+++ summarization/kobart/data_loader/processing.py
@@ -1,13 +1,7 @@
 import nltk
 
-def preprocess_function(examples, tokenizer, max_source_length, max_target_length, padding,
-                        use_t5:bool = False, prefix:str = None):
+def preprocess_function(examples, tokenizer, max_source_length, max_target_length, padding):
     inputs, targets = examples['dialogue'], examples['summary']
-
-    # input에 prefix 추가 (t5 계열 모델만 prefix 사용함)
-    # prefix 추가한 input text를 tokenizer 통해 id로 변경 == model_inputs
-    if use_t5:
-        inputs = [prefix + inp for inp in inputs]
 
     model_inputs = tokenizer(inputs, max_length=max_source_length, padding=padding, truncation=True)
 
